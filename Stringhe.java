aa1cfb54a437614c0a8545cc3dbf850c066ec0d4
informaticaopensource <91938620+informaticaopensource@users.noreply.github.com>
2026-01-17 11:14:55 +0100

    Create Stringhe.java
    Classe che memorizza tre Stringhe e le stampa in ordine crescente

---
+++ Stringhe.java
@@ -0,0 +1,38 @@
+strA=x;
+        strB=y;
+        strC=z;
+    }
+    public String getstrA(){return strA;}
+    public void setstrA(String x){ strA=x;}
+    public String getstrB(){return strB;}
+    public void setstrB(String x){ strB=x;}
+    public String getstrC(){return strC;}
+    public void setstrC(String x){ strC=x;}
+    public void OrdinaStringhe()
+    {
+        if (strA.compareToIgnoreCase(strB)<0)
+            if (strA.compareToIgnoreCase(strC)<0)
+                if (strB.compareToIgnoreCase(strC)<0)
+                    System.out.println(strA+","+strB+","+strC);
+                else
+                        System.out.println(strA+","+strC+","+strB);
+            else   
+                if (strB.compareToIgnoreCase(strC)<0)
+                    if (strA.compareToIgnoreCase(strC)<0)
+                        System.out.println(strB+","+strA+","+strC);
+                    else
+                        System.out.println(strB+","+strC+","+strA);
+                else   
+                    if (strB.compareToIgnoreCase(strA)<0)
+                        System.out.println(strC+","+strB+","+strA);
+                    else
+                        System.out.println(strC+","+strA+","+strB);
+    }
+public String toString()
+{
+    return strA+","+strB+","+strC+"\n";
+}
+    
+}
+
+
