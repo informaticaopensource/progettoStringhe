25325cdf414eebe5ec2aed5577d85d88973cfc07
informaticaopensource <91938620+informaticaopensource@users.noreply.github.com>
2026-01-17 05:42:28 -0800

    Update Test.java
    Classe Test Corretta !

--- Test.java
+++ Test.java
@@ -3,13 +3,13 @@ class Test
 {
     public static void main(String[] args)
     {
-        String a="Ciao Pasquale";
-        String b="Ciao Nicola !";
-        String c="Ciao Maria come Stai ?";
+        String a="Ciao Antonia";
+        String b="Ciao Maria !";
+        String c="Ciao Giovanni 1 2 3 !";
         Stringhe st=new Stringhe (a,b,c);
         st.OrdinaStringhe();
-        System.out.println(st.toString());
-        System.out.println("***********************************************");
+        
+     /  System.out.println("***********************************************");
         System.out.println("Richiesta inserimento dati:");
         System.out.println("***********************************************");
         Scanner sc = new Scanner(System.in);
@@ -20,6 +20,6 @@ class Test
         Stringhe st1= new Stringhe(a,b,c);
         st1.OrdinaStringhe();
         System.out.println("Arrivederci !");
+        sc.close();;
     }
-
 }
